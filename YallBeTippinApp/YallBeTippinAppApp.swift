b52a85a0f6076010c08c20510c46b80fb3b2d754
AbdiRaqiibMohummed <aclipz0121@gmail.com>
2025-09-03 15:18:49 +0100

    Initial Commit

---
+++ YallBeTippinApp/YallBeTippinAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YallBeTippinAppApp.swift
+//  YallBeTippinApp
+//
+//  Created by Abdiraqiib Hassan on 03/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct YallBeTippinAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
